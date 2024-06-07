f196877fd968345f0b42bb1193f2d63327ed2ee6
GPitta16 <159978299+GPitta16@users.noreply.github.com>
2024-06-07 00:45:59 -0600

    Add files via upload

---
+++ GYMMMM/GYMMMM/Form3.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace GYMMMM
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.checkedListBox3 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.checkedListBox4 = new System.Windows.Forms.CheckedListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.materialTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // materialTabControl1
+            // 
+            this.materialTabControl1.CausesValidation = false;
+            this.materialTabControl1.Controls.Add(this.tabPage1);
+            this.materialTabControl1.Controls.Add(this.tabPage2);
+            this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.materialTabControl1.Location = new System.Drawing.Point(3, 64);
+            this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl1.Multiline = true;
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(1044, 489);
+            this.materialTabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox7);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1036, 460);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Perfil";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(31, 31);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(230, 377);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 19;
+            this.pictureBox7.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(698, 169);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(165, 40);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Numero de Tarjeta\r\n12390402019230";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(698, 95);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 40);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Plan\r\nPersonal\r\n";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(698, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(154, 60);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Fecha de Ingreso\r\n7 Meses\r\n\r\n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(698, 247);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 60);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Horario\r\n14:00-16:30\r\n15:00-17:00\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(300, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 40);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Membresia\r\nSmart TV basico\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(300, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 40);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Lugar de Entrenamiento\r\nSmart Fit Plaza Portales\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(300, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 40);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Edad \r\n20 Años";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(300, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(182, 60);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Usuario\r\nYair Vasquez Castro\r\n\r\n";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.monthCalendar1);
+            this.tabPage2.Controls.Add(this.label11);
+            this.tabPage2.Controls.Add(this.label10);
+            this.tabPage2.Controls.Add(this.checkedListBox3);
+            this.tabPage2.Controls.Add(this.checkedListBox2);
+            this.tabPage2.Controls.Add(this.label9);
+            this.tabPage2.Controls.Add(this.checkedListBox4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1036, 460);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Ejercicios";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(392, 244);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 31);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Agregar\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(6, 236);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(755, 3);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(42, 16);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Brazo";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(419, 3);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(89, 16);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Tren Superior";
+            // 
+            // checkedListBox3
+            // 
+            this.checkedListBox3.FormattingEnabled = true;
+            this.checkedListBox3.Items.AddRange(new object[] {
+            "Press banca",
+            "Press Inclinado",
+            "Mariposa Maquina",
+            "Mariposa Polea",
+            "Dominadas Asistidas",
+            "Jalones al Pecho con agarre pala",
+            "Jalones al pecho con agarre triangulo",
+            "Remo Maquina",
+            "Encogimiento de Trapecios"});
+            this.checkedListBox3.Location = new System.Drawing.Point(359, 22);
+            this.checkedListBox3.Name = "checkedListBox3";
+            this.checkedListBox3.Size = new System.Drawing.Size(257, 208);
+            this.checkedListBox3.TabIndex = 8;
+            this.checkedListBox3.SelectedIndexChanged += new System.EventHandler(this.checkedListBox3_SelectedIndexChanged);
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "Curl de Bisep con polea",
+            "Curl de Bisep Barra z",
+            "curl de Bisep Maquina",
+            "Fondos Asistidos",
+            "Press Frances",
+            "Extension de triceps polea ",
+            "Lateral polea",
+            "Press Militar",
+            "Hombro posterior Maquina"});
+            this.checkedListBox2.Location = new System.Drawing.Point(682, 22);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(220, 208);
+            this.checkedListBox2.TabIndex = 7;
+            this.checkedListBox2.SelectedIndexChanged += new System.EventHandler(this.checkedListBox2_SelectedIndexChanged_1);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(101, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 16);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Pierna";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // checkedListBox4
+            // 
+            this.checkedListBox4.FormattingEnabled = true;
+            this.checkedListBox4.Items.AddRange(new object[] {
+            "Sentadilla Libre",
+            "Sentadilla Maquina hack",
+            "Extension de Cuadriceps",
+            "Sentadilla Bulgara",
+            "Dezplantes",
+            "Prensa",
+            "Femoral Sentado",
+            "Femoral acostado",
+            "Peso Muerto",
+            "Maquina costurera",
+            "Extension de gemelos Maquina hack"});
+            this.checkedListBox4.Location = new System.Drawing.Point(6, 22);
+            this.checkedListBox4.Name = "checkedListBox4";
+            this.checkedListBox4.Size = new System.Drawing.Size(252, 208);
+            this.checkedListBox4.TabIndex = 4;
+            this.checkedListBox4.SelectedIndexChanged += new System.EventHandler(this.checkedListBox4_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(600, 256);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 22);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 556);
+            this.Controls.Add(this.materialTabControl1);
+            this.DrawerTabControl = this.materialTabControl1;
+            this.Name = "Form3";
+            this.Text = "MIPERFIL";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.materialTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckedListBox checkedListBox4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.CheckedListBox checkedListBox3;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ GYMMMM/GYMMMM/Form3.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GYMMMM
+{
+    public partial class Form3 : MaterialSkin.Controls.MaterialForm
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            LoadDataGridView();
+        }
+
+        private void LoadDataGridView()
+        {
+            DataTable dt = new DataTable();
+
+
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+ 
+             
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+          
+       
+        }
+
+        private void checkedListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void checkedListBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Código para manejar la selección de elementos en el CheckedListBox3
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Código para manejar el evento de clic en el Button3
+        }
+
+        private void checkedListBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Código para manejar la selección de elementos en el CheckedListBox4
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                List<string> selectedItems = new List<string>();
+
+                foreach (object item in checkedListBox2.CheckedItems)
+                
+                    selectedItems.Add(item.ToString());
+                
+
+                string message = "Selected Items:\n" + string.Join("\n", selectedItems);
+                MessageBox.Show(message, "Items Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            {
+                List<string> selectedItems = new List<string>();
+
+                foreach (object item in checkedListBox3.CheckedItems)
+                
+                    selectedItems.Add(item.ToString());
+                
+
+                string message = "Selected Items:\n" + string.Join("\n", selectedItems);
+                MessageBox.Show(message, "Items Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            {
+                List<string> selectedItems = new List<string>();
+
+                foreach (object item in checkedListBox4.CheckedItems)
+                
+                   selectedItems.Add(item.ToString());
+                
+
+                string message = "Selected Items:\n" + string.Join("\n", selectedItems);
+                MessageBox.Show(message, "Items Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                string Message = "Selected Items:\n" + string.Join("\n", selectedItems);
+                label1.Text = Message;
+            }
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+            {
+                List<string> selectedItems = new List<string>();
+
+                foreach (object item in checkedListBox2.CheckedItems)
+
+                    selectedItems.Add(item.ToString());
+
+
+                string message = "Selected Items:\n" + string.Join("\n", selectedItems);
+                MessageBox.Show(message, "Items Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            {
+                List<string> selectedItems = new List<string>();
+
+                foreach (object item in checkedListBox3.CheckedItems)
+
+                    selectedItems.Add(item.ToString());
+
+
+                string message = "Selected Items:\n" + string.Join("\n", selectedItems);
+                MessageBox.Show(message, "Items Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            {
+                List<string> selectedItems = new List<string>();
+
+                foreach (object item in checkedListBox4.CheckedItems)
+
+                    selectedItems.Add(item.ToString());
+
+
+                string message = "Selected Items:\n" + string.Join("\n", selectedItems);
+                MessageBox.Show(message, "Items Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                string Message = "Selected Items:\n" + string.Join("\n", selectedItems);
+                label1.Text = Message;
+            }
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            {
+                List<string> selectedItems = new List<string>();
+
+                foreach (object item in checkedListBox2.CheckedItems)
+
+                    selectedItems.Add(item.ToString());
+
+
+                string message = "Selected Items:\n" + string.Join("\n", selectedItems);
+                MessageBox.Show(message, "Items Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            {
+                List<string> selectedItems = new List<string>();
+
+                foreach (object item in checkedListBox3.CheckedItems)
+
+                    selectedItems.Add(item.ToString());
+
+
+                string message = "Selected Items:\n" + string.Join("\n", selectedItems);
+                MessageBox.Show(message, "Items Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            {
+                List<string> selectedItems = new List<string>();
+
+                foreach (object item in checkedListBox4.CheckedItems)
+
+                    selectedItems.Add(item.ToString());
+
+
+                string message = "Selected Items:\n" + string.Join("\n", selectedItems);
+                MessageBox.Show(message, "Items Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                string Message = "Selected Items:\n" + string.Join("\n", selectedItems);
+                label1.Text = Message;
+            }
+            { textBox1.Text = button1.Text; 
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+                
+        }
+
+        private void checkedListBox2_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
+        
+   
